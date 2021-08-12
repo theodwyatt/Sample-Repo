04db1bdea35317edaceb1c4009e0f14099747e64
The "Teddy Wyatt <theodwya@amazon.com>
2021-08-12 15:28:37 -0400

    new projects

---
+++ code/clear_screen_script.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.9
+
+def cls():
+    import os
+    os.system('cls')
+    return
---
+++ code/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.9
+
+# This is a full line comment
+
+print("Hello, World!") #this is a trailing comment 
+
+""" 
+this is not a block comment
+this multiline string
+"""
